b2c2bbf539a1dd49462ffef29605d0edeb1a43a1
Asumji <64544378+Asumji@users.noreply.github.com>
2022-09-28 18:03:41 +0200

    made the gamefield scalable
    just set the size variable to the amount of extra fields you want (will always be a square (aka size 0 is 3x3, size 1 is 4x4 etc.)) not sure why you would do that tho since the person who starts can always guarantee a win on higher sizes but coding practice good :)

--- index.py
+++ index.py
@@ -1,24 +1,36 @@
 player = "X"
 enemy = "O"
+size = 0
 
 def turn(who):
     turnActive = True
     while turnActive == True:
         i = input("Where would you like to place your " + who + "? ")
         if (i.isnumeric()):
-            if (gamefield[int(i) - 1] == "_" or gamefield[int(i) - 1] == " "):
-                gamefield[int(i) - 1] = who
-                turnActive = False
+            if (int(i) <= len(gamefield) and int(i) >= 0):
+                if (gamefield[int(i) - 1] == "_" or gamefield[int(i) - 1] == " "):
+                    gamefield[int(i) - 1] = who
+                    turnActive = False
+                else:
+                    print("That spot is already taken!")
             else:
-                print("That spot is already taken!")
+                print("Enter a number 1 - " + str(len(gamefield)) + ".")
         else:
-            print("Enter a number 1 - 9.")
+            print("Enter a valid number.")
 
 def buildField(gameArray):
     string = ""
+    topRow = 0
+    loopFinish = False
+    for i in range(len(gameArray)):
+        if (i!=0 and i!=1):
+            if(len(gameArray) / i == i and loopFinish == False):
+                loopFinish = True
+                topRow = i
+
     index = 0
     for i in gameArray:
-        if (index % 3 == 0):
+        if (index % topRow == 0):
             index += 1
             string = string + "\n" + i
         else:
@@ -27,37 +39,69 @@ def buildField(gameArray):
     string = string + "\n"
     print(string)
 
+invalidArrayR = []
+invalidArrayL = []
 def checkForWin(gameArray, user):
-    for i in range(9):
+    topRow = 0
+    loopFinish = False
+    for i in range(len(gameArray)):
+        if (i!=0 and i!=1):
+            if(len(gameArray) / i == i and loopFinish == False):
+                loopFinish = True
+                topRow = i
+
+    for i in range(len(gameArray)):
         #vertical
-        if (i == 0 or i == 1 or i == 2):
-            if (gameArray[i] == user and gameArray[i + 3] == user and gameArray[i + 6] == user):
+        if (i / topRow < topRow - 2):
+            if (gameArray[i] == user and gameArray[i + topRow] == user and gameArray[i + topRow * 2] == user):
                 print("\033[92m" + user + " won!\033[0m")
                 return True
 
         #horizontal
-        if (i == 0 or i == 3 or i == 6):
+        if ((i + 1) % topRow == 0):
+            if (not invalidArrayR.__contains__(i)):
+                invalidArrayR.append(i - 1)
+                invalidArrayR.append(i)
+        if (not invalidArrayR.__contains__(i)):
             if (gameArray[i] == user and gameArray[i + 1] == user and gameArray[i + 2] == user):
                 print("\033[92m" + user + " won!\033[0m")
                 return True
-
+        
         #diagonal
-        if (i == 0):
-            if (gameArray[i] == user and gameArray[i + 4] == user and gameArray[i + 8] == user):
-                print("\033[92m" + user + " won!\033[0m")
-                return True
-        elif (i == 2):
-            if (gameArray[i] == user and gameArray[i + 2] == user and gameArray[i + 4] == user):
-                print("\033[92m" + user + " won!\033[0m")
-                return True
+        if (not invalidArrayR.__contains__(i)):
+            if (i / topRow < topRow - 2):
+                if (gameArray[i] == user and gameArray[i + topRow + 1] == user and gameArray[i + topRow * 2 + 2] == user):
+                    print("\033[92m" + user + " won!\033[0m")
+                    return True
+        if (i % topRow == 0):
+            if (not invalidArrayL.__contains__(i)):
+                invalidArrayL.append(i)
+                invalidArrayL.append(i + 1)
+        if (not invalidArrayL.__contains__(i)):
+            if (i / topRow < topRow - 2):
+                if (gameArray[i] == user and gameArray[i + topRow - 1] == user and gameArray[i + topRow * 2 - 2] == user):
+                    print("\033[92m" + user + " won!\033[0m")
+                    return True
 
 while True:
     win = False
     gamefield = [
                 "_", "_", "_",
-                "_", "_", "_",
-                " ", " ", " "
+                "_", "_", "_", 
+                " ", " ", " ",
                 ]
+
+    #add horizontal fields
+    for i in range(size):
+        for i in range(2):
+            gamefield.insert(0,"_")
+        gamefield.insert(len(gamefield) - 1," ")
+
+    #add vertical fields
+    for i in range(size):
+        for j in range(size + 3):
+            gamefield.insert(0,"_")
+
     buildField(gamefield)
     while win == False:
         if (not gamefield.__contains__("_") and not gamefield.__contains__(" ")):
